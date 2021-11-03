0ddade4b2c9cd0c7458dfabad0918de89336df65
Katriel Albuquerque Galvão de Araújo <katrielaraujo@gmail.com>
2021-11-03 17:35:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+//get all keys
+const keys = document.querySelectorAll(".key");
+
+
+//play notes
+function playNote(event){
+    //keyCode
+
+    //type or pressed key
+
+    //if key exists
+
+    //play audio
+}
+//click with mouse
+keys.forEach( key => key.addEventListener("click",playNote));
+//keyboard type
+
+window.addEventListener("keydown",playNote);
